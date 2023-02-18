5606f8f223d5affd1b0ffbe3546f943ca2ac4129
Inkyu Park <badugiss@gmail.com>
2023-02-18 13:25:25 +0900

    - add dark mode

---
+++ DarkMode.spoon/init.lua
@@ -0,0 +1,58 @@
+--- === ExitOnClose ===
+---
+--- Exit a program when it's last window has closed
+---
+--- Download: https://...
+local obj = {}
+obj.__index = obj
+
+-- Metadata
+obj.name = "DarkMode"
+obj.version = "0.1"
+obj.author = "Inkyu Park <badugiss@gmail.com>"
+obj.homepage = "https://github.com/badugiss/Spoons"
+obj.license = "MIT - https://opensource.org/licenses/MIT"
+
+--- DarkMode.logger
+--- Variable
+--- Logger object
+obj.logger = hs.logger.new(obj.name)
+
+--- DarkMode:change()
+--- Method
+--- Set dark mode
+function obj:change(isDarkMode)
+  hs.osascript.applescript(string.format([[
+      tell application "System Events"
+          tell appearance preferences
+              set dark mode to %s
+          end tell
+      end tell
+  ]], isDarkMode))
+end
+
+--- DarkMode:toggle()
+--- Method
+--- Toggle dark mode
+function obj:toggle()
+  hs.osascript.applescript([[
+      tell application "System Events"
+          tell appearance preferences
+              set dark mode to not dark mode
+          end tell
+      end tell
+  ]])
+end
+
+--- DarkMode:bindHotKeys()
+--- Method
+--- Bind hotkeys
+function obj:bindHotkeys(mapping)
+    hs.spoons.bindHotkeysToSpec({
+        change = hs.fnutils.partial(self.change, self),
+        toggle = hs.fnutils.partial(self.toggle, self),
+    }, mapping)
+    return self
+end
+
+return obj
