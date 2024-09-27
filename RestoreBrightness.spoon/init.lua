04573a52aac79d12b6f2bea9b83df0f2be78d887
Inkyu Park <badugiss@gmail.com>
2024-09-27 09:22:34 +0900

    밝기 복원시 딜레이 추가

--- RestoreBrightness.spoon/init.lua
+++ RestoreBrightness.spoon/init.lua
@@ -1,6 +1,10 @@
 local obj = {}
 obj.__index = obj
 
+obj.delaySeconds = 1
+
+obj.brightness = 100
+
 local PowerSource <const> = {
   AC = 'AC Power',
   Battery = 'Battery Power',
@@ -8,7 +12,6 @@ local PowerSource <const> = {
 }
 
 function obj:init()
-  self.brightness = 100
   self.prevPowerSource = hs.battery.powerSource()
 
   self.watcher = hs.battery.watcher.new(function()
@@ -20,7 +23,22 @@ function obj:onBatteryChange()
   local powerSource = hs.battery.powerSource()
 
   if powerSource == PowerSource.AC and powerSource ~= self.prevPowerSource then
-    hs.brightness.set(self.brightness)
+    if self.timer then
+      return
+    end
+
+    self.timer = hs.timer.doAfter(self.delaySeconds, function()
+      if self.timer then
+        self.timer:stop()
+        self.timer = nil
+
+        if hs.battery.powerSource() == PowerSource.AC then
+          hs.brightness.set(self.brightness)
+        end
+      end
+    end)
+
+    self.timer:start()
   end
 
   self.prevPowerSource = powerSource
